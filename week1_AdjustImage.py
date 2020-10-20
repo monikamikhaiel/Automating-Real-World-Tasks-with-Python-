41354bc912523b31d287a7981d77170530ea8333
monikamikhaiel <moniquamikhaiel@yahoo.com>
2020-10-20 23:57:53 +0200

    Fixing the directory to be /opt/icons , using glob to edit only tiff files

--- week1_AdjustImage.py
+++ week1_AdjustImage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from PIL import Image
 import os
-OldImages=os.listdir() # a list of all images names
-#NewImages=[ "/opt/icons/"+i for i in OldImages ]
-#print(NewImages)
-for i in range(len(OldImages)):
-  im=Image.open(OldImages[i])
+from glob import glob
+#OldImages=os.listdir() # a list of all images names
+for i in glob("*.tiff"):
+  im=Image.open(i)
   im.resize((128,128)).rotate(90)
-  im.save("/home/student-00-6d8122963d8f/opt/icons/"+OldImages[i])
+  im.save("/opt/icons/"+i[:len(i)-1-4]+"jpeg")
