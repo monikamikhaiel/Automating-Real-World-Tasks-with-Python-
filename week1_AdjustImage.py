fd4d53a9a47faf484a10827e8ff32761a3746ec1
monikamikhaiel <moniquamikhaiel@yahoo.com>
2020-10-21 02:38:33 +0200

    add URL and json content ,read files from specific directory

--- week1_AdjustImage.py
+++ week1_AdjustImage.py
@@ -4,6 +4,6 @@ import os
 from glob import glob
 #OldImages=os.listdir() # a list of all images names
 for i in glob("*.tiff"):
-  im=Image.open(i)
+  im=Image.open(i).convert("RGB")
   im.resize((128,128)).rotate(90)
   im.save("/opt/icons/"+i[:len(i)-1-4]+"jpeg")
--- week2_WebDisplay.py
+++ week2_WebDisplay.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import requests
-#import os
-from glob import glob
+import os
 list=[]
 dict={}
-for file in glob("*.txt"):
-  f=open(file)
-  dict["title"]=file.readline()
-  dict["name"]=file.readline()
-  dict["date"]=file.readline()
-  dict["feedback"]=file.read()
+files=os.listdir("/data/feedback")
+#print(files)
+for file in files:
+  f=open("/data/feedback/"+file)
+  dict["title"]=f.readline().strip()
+  dict["name"]=f.readline().strip()
+  dict["date"]=f.readline().strip()
+  dict["feedback"]=f.read().strip()
   f.close()
   list.append(dict)
   dict={}
-response=requests.post(,data=list)
-print(response.status_code)
+#print(list)
+for i in range(len(list)):
+  response=requests.post("http://34.72.12.7/feedback",json=list[i])
+  print(response.request.body)
+  print(response.status_code)
+
