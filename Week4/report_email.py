8274de2ed9f232a360b23e2a51ca15339756f374
monikamikhaiel <moniquamikhaiel@yahoo.com>
2020-10-22 15:44:52 +0200

    attach the generated pdf into an email message and send it

--- Week4/report_email.py
+++ Week4/report_email.py
@@ -1,4 +1,21 @@
-import report
+#!/usr/bin/env python3
+import reports
 import emails
 from datetime import date
-
+import run 
+if __name__="__main__":
+  paragraph=run.RetList()
+  ##report generation
+  today=date.today()
+  d1 = today.strftime("%m %d,%Y")
+  title="Processed Update on {}".fromat(d1)
+  path="/tmp/processed.pdf"
+  reports.generate_report(path, title,paragraph)
+    ## email generation 
+  sender="automation@example.com"
+  recepient="@example.com"
+  attach_path=path
+  subject="Upload Completed - Online Fruit Store"
+  body="All fruits are uploaded to our website successfully. A detailed list is attached to this email." 
+  message=emails.generate_email(sender,recepient,subject,body,attach_path)
+  emails.send_email(message)
