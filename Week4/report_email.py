1f4b86bf89c168f7b9d718f76f007c61fdd88059
monikamikhaiel <moniquamikhaiel@yahoo.com>
2020-10-22 18:04:25 +0200

    fix the month name

--- Week4/report_email.py
+++ Week4/report_email.py
@@ -2,20 +2,21 @@
 import reports
 import emails
 from datetime import date
-import run 
-if __name__="__main__":
+import run
+if __name__=="__main__":
   paragraph=run.RetList()
   ##report generation
   today=date.today()
-  d1 = today.strftime("%m %d,%Y")
-  title="Processed Update on {}".fromat(d1)
+  d1 = today.strftime("%B %d,%Y")
+  title="Processed Update on {}".format(d1)
   path="/tmp/processed.pdf"
   reports.generate_report(path, title,paragraph)
-    ## email generation 
+    ## email generation
   sender="automation@example.com"
-  recepient="@example.com"
+  recepient="student-04-c8be53d51229@example.com"
   attach_path=path
   subject="Upload Completed - Online Fruit Store"
-  body="All fruits are uploaded to our website successfully. A detailed list is attached to this email." 
+  body="All fruits are uploaded to our website successfully. A detailed list is attached to this email."
   message=emails.generate_email(sender,recepient,subject,body,attach_path)
   emails.send_email(message)
+
